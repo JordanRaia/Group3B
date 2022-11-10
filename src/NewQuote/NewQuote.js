b36b059deb39b1c5bf3c202617584be1c105fdbd
z1860207 <clipper5813@aol.com>
2022-11-09 23:18:13 -0600

    axiostry1

--- src/NewQuote/NewQuote.js
+++ src/NewQuote/NewQuote.js
@@ -4,10 +4,18 @@ import axios from "axios";
 
 function NewQuote()  {
     //return <div className="new">NewQuote</div>;
-    const [customers, setCustomers] = useState();
+    const [customers, setCustomers] = useState([]);
+    useEffect(() => {
+        getCustomers();
+    },[]);
+    function getCustomers(){
+        axios.get('https://students.cs.niu.edu/~z1860207/legacy.php').then(function(response){
+            console.log(response.data);
+            setCustomers(resonse.data);
+        });
+    }
 
     return <div classname= "app-container">
-        <h1>                            Customers</h1>
         <table> 
             <thead> 
                 <tr>
@@ -19,6 +27,25 @@ function NewQuote()  {
                 </tr>
             </thead>
             <tbody>
+                {customers.map((customer, key) =>
+                            <tr key={key}>
+                                <td>{customer.id}</td>
+                                <td>{customer.name}</td>
+                                <td>{customer.city}</td>
+                                <td>{customer.street}</td>
+                                <td>{customer.contact}</td>
+                                <td>
+                                    <Link to={`customer/${customer.id}/edit`} style={{marginRight: "10px"}}>Edit</Link>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        )}
+
+            </tbody>
+        </table>
+    </div>;
+}
+/* //PRE- axios table body:
                 <tr>
                     <td>12</td>
                     <td>Jay Gordon</td>
@@ -26,9 +53,5 @@ function NewQuote()  {
                     <td>cherry blossom st</td>
                     <td>jgordon12@gmail.com</td>
                 </tr>
-            </tbody>
-        </table>
-    </div>;
-}
-
+*/
 export default NewQuote;
