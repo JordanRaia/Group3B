779a7e61f8405672606f1b0121d17890fd5bc847
z1860207 <clipper5813@aol.com>
2022-11-09 20:37:41 -0600

    init legacy display

--- src/NewQuote/NewQuote.js
+++ src/NewQuote/NewQuote.js
@@ -2,7 +2,30 @@ import React from "react";
 import "./NewQuote.css";
 
 function NewQuote() {
-    return <div className="new">NewQuote</div>;
+    //return <div className="new">NewQuote</div>;
+
+    return <div classname= "app-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Name</th>
+                    <th>City</th>
+                    <th>Street</th>
+                    <th>Contact</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>12</td>
+                    <td>Jay Gordon</td>
+                    <td>San Francisco</td>
+                    <td>cherry blossom st</td>
+                    <td>jgordon12@gmail.com</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>;
 }
 
 export default NewQuote;
