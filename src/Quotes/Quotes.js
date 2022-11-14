da348fe1ee88ca6f4785a01b1624c3a8e1422256
z1860207 <clipper5813@aol.com>
2022-11-13 23:18:56 -0600

    git merging

--- src/Quotes/Quotes.js
+++ src/Quotes/Quotes.js
@@ -1,21 +1,8 @@
 import React from "react";
 import "./Quotes.css";
-import {useLocation} from 'react-router-dom';
 
 function Quotes(props) {
-    //return <div className="quotes">Quotes</div>;
-
-    const location = useLocation();
-
-    const { state } = this.props.location;
-    return ( 
-        <>
-            {console.log(state.id)};
-            {console.log(location.name)};
-            <div>{state.id}</div>
-            <div>{location.name}</div>
-        </>
-    )
+    return <div className="quotes">Quotes</div>;
 }
 
 export default Quotes;
