db17da2c72a43605d7e63dc0bd58fb8bd3e891bb
JordanRaiaNIU <z1887657@students.niu.edu>
2022-11-17 19:21:35 -0600

    fixed warnings in Administration.js

--- src/Administration/Administration.js
+++ src/Administration/Administration.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import "./Administration.css";
 import { useNavigate } from "react-router-dom";
 import { onAuthStateChanged } from "firebase/auth";
@@ -9,10 +9,10 @@ import {
     ref as dbRef,
     query,
     equalTo,
-    set
+    set,
 } from "firebase/database";
 import TextField from "@mui/material/TextField";
-import Button from "@mui/material/Button";
+// import Button from "@mui/material/Button";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 // page for administrator to view all users of the site and be able to change their permissions,
@@ -24,20 +24,27 @@ const Administration = () => {
     const [name, setName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    useEffect(() => {authState();}, []);
+    useEffect(() => {
+        authState();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
     const authState = () => {
         onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser);
             if (user) {
-                const usersRef = query(dbRef(db, `users`), orderByChild("rank"), equalTo("salesAssociate"))
+                const usersRef = query(
+                    dbRef(db, `users`),
+                    orderByChild("rank"),
+                    equalTo("salesAssociate")
+                );
                 onValue(usersRef, (snapshot) => {
                     const data = snapshot.val();
-                    const cleanedData = Object.values(data)
+                    const cleanedData = Object.values(data);
                     setUsers(cleanedData);
                 });
             }
         });
-    }
+    };
     const writeUserData = (userId, name, email, imageUrl, rank) => {
         set(dbRef(db, "users/" + userId), {
             fullname: name,
@@ -45,59 +52,58 @@ const Administration = () => {
             profile_picture: imageUrl,
             rank: rank,
         });
-    }
+    };
     const register = async (e) => {
         e.preventDefault(); //prevent page from refreshing
 
         await createUserWithEmailAndPassword(auth, email, password)
-        .then((userCredential) => {
-            if (auth) {
-                //creating account was successful
+            .then((userCredential) => {
+                if (auth) {
+                    //creating account was successful
 
-                let profileUrl = "users/default/profile_pic.png";
+                    let profileUrl = "users/default/profile_pic.png";
 
-                //add userdata to database
-                writeUserData(
-                    userCredential.user.uid,
-                    name,
-                    email,
-                    profileUrl,
-                    "salesAssociate"
-                );
-                resetFields();
-                navigate("/Administration");
-            }
-        })
-        //creating an account was unsuccessful and alerting user with error message
-        .catch((error) => alert(error.message));
-    }
+                    //add userdata to database
+                    writeUserData(
+                        userCredential.user.uid,
+                        name,
+                        email,
+                        profileUrl,
+                        "salesAssociate"
+                    );
+                    resetFields();
+                    navigate("/Administration");
+                }
+            })
+            //creating an account was unsuccessful and alerting user with error message
+            .catch((error) => alert(error.message));
+    };
     const resetFields = () => {
         setName("");
         setEmail("");
         setPassword("");
-    }
+    };
     return (
         <div className="admin">
             <div className="adminTitle">Administrator</div>
-            <div className="divider"/>
-            {
-                users ? (
-                    <div className="associateList">
-                        {
-                            users.map((item) => {
-                                console.log(item.fullname);
-                                return (
-                                <div className="associateNode">
-                                    <div className="salesAssociates">{item.fullname}</div>
-                                    <div className="commission">Commission: </div>
-                                    <div className="commissionAmt">$500.50</div>
-                                    <div className="userRank">Associate</div>
-                                    <button className="editButton">Edit</button>
-                                    <button className="deleteButton">Delete</button>
+            <div className="divider" />
+            {users ? (
+                <div className="associateList">
+                    {users.map((item) => {
+                        console.log(item.fullname);
+                        return (
+                            <div className="associateNode">
+                                <div className="salesAssociates">
+                                    {item.fullname}
                                 </div>
-                                )
-                            })
-                        }
+                                <div className="commission">Commission: </div>
+                                <div className="commissionAmt">$500.50</div>
+                                <div className="userRank">Associate</div>
+                                <button className="editButton">Edit</button>
+                                <button className="deleteButton">Delete</button>
+                            </div>
+                        );
+                    })}
                     {/* <div className="associateNode">
                         <div className="salesAssociates">Gerald Ellsworth</div>
                         <div className="commission">Commission: </div>
@@ -107,11 +113,10 @@ const Administration = () => {
                         <button className="deleteButton">Delete</button>
                     </div> */}
                 </div>
-                ) : (
-                    <div> Loading... </div>
-                )
-            }
-            <div className="divider"/>
+            ) : (
+                <div> Loading... </div>
+            )}
+            <div className="divider" />
             <div className="newUser">
                 <div className="name field">
                     <TextField
@@ -139,15 +144,10 @@ const Administration = () => {
                         type="password"
                         sx={{ mb: "30px" }}
                         value={password}
-                        onChange={(e) =>
-                            setPassword(e.target.value)
-                        }
+                        onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <button
-                    className="login__button"
-                    onClick={register}
-                >
+                <button className="login__button" onClick={register}>
                     <div className="login__buttonContainer">
                         <div className="login__buttonText">
                             Create Sales Associate
@@ -156,7 +156,7 @@ const Administration = () => {
                 </button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Administration;
