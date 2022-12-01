5cf202c31645df337d1a6660ccdc5062c8bc26c5
JordanRaiaNIU <z1887657@students.niu.edu>
2022-11-30 22:27:23 -0600

    added permission checking to home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,9 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Home.css";
 import Logo from "../logo/3B-logos_black.png";
 import { Link } from "react-router-dom";
+// firebase
+import { onAuthStateChanged } from "firebase/auth";
+import { onValue, ref as dbRef } from "firebase/database";
+import { auth, db } from "../firebase";
 
 function Home() {
+    const [user, setUser] = useState({});
+    const [rank, setRank] = useState("");
+
+    onAuthStateChanged(auth, (currentUser) => {
+        setUser(currentUser);
+
+        if (user) {
+            const rankRef = dbRef(db, `users/${currentUser.uid}/rank`);
+
+            onValue(rankRef, (snapshot) => {
+                //set data to name
+                const data = snapshot.val();
+                //save name to useState name
+                setRank(data);
+                console.log(data);
+            });
+        } else {
+            setRank("");
+        }
+    });
+
     return (
         <div className="home_background">
             <div className="home">
@@ -17,51 +42,63 @@ function Home() {
                     </div>
                 </div>
                 <div className="cardContainer">
-                    <Link
-                        className="card"
-                        to={"/NewQuote"}
-                        style={{ textDecoration: "none" }}
-                    >
-                        <img
-                            className="cardImage"
-                            src={require("../Assets/image1.png")}
-                            alt=""
-                        />
-                        <div className="cardContent">
-                            <div className="cardHeader">New Quote</div>
-                            <p className="cardDesc">Create itemized quotes</p>
-                        </div>
-                    </Link>
-                    <Link
-                        className="card"
-                        to={"/FinalizeQuote"}
-                        style={{ textDecoration: "none" }}
-                    >
-                        <img
-                            className="cardImage"
-                            src={require("../Assets/image3.png")}
-                            alt=""
-                        />
-                        <div className="cardContent">
-                            <div className="cardHeader">Finalize Quote</div>
-                            <p className="cardDesc">Edit and finalize quotes</p>
-                        </div>
-                    </Link>
-                    <Link
-                        className="card"
-                        to={"/SanctionQuote"}
-                        style={{ textDecoration: "none" }}
-                    >
-                        <img
-                            className="cardImage"
-                            src={require("../Assets/image2.png")}
-                            alt=""
-                        />
-                        <div className="cardContent">
-                            <div className="cardHeader">Sanction Quote</div>
-                            <p className="cardDesc">Process finalized quotes</p>
-                        </div>
-                    </Link>
+                    {(rank === "sales" || rank === "dev") && (
+                        <Link
+                            className="card"
+                            to={"/NewQuote"}
+                            style={{ textDecoration: "none" }}
+                        >
+                            <img
+                                className="cardImage"
+                                src={require("../Assets/image1.png")}
+                                alt=""
+                            />
+                            <div className="cardContent">
+                                <div className="cardHeader">New Quote</div>
+                                <p className="cardDesc">
+                                    Create itemized quotes
+                                </p>
+                            </div>
+                        </Link>
+                    )}
+                    {(rank === "inhouse1" || rank === "dev") && (
+                        <Link
+                            className="card"
+                            to={"/FinalizeQuote"}
+                            style={{ textDecoration: "none" }}
+                        >
+                            <img
+                                className="cardImage"
+                                src={require("../Assets/image3.png")}
+                                alt=""
+                            />
+                            <div className="cardContent">
+                                <div className="cardHeader">Finalize Quote</div>
+                                <p className="cardDesc">
+                                    Edit and finalize quotes
+                                </p>
+                            </div>
+                        </Link>
+                    )}
+                    {(rank === "inhouse2" || rank === "dev") && (
+                        <Link
+                            className="card"
+                            to={"/SanctionQuote"}
+                            style={{ textDecoration: "none" }}
+                        >
+                            <img
+                                className="cardImage"
+                                src={require("../Assets/image2.png")}
+                                alt=""
+                            />
+                            <div className="cardContent">
+                                <div className="cardHeader">Sanction Quote</div>
+                                <p className="cardDesc">
+                                    Process finalized quotes
+                                </p>
+                            </div>
+                        </Link>
+                    )}
                 </div>
             </div>
         </div>
