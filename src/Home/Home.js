9f79e5d1c92ffa38786dae6fa7669b1e4314e4fa
JordanRaiaNIU <z1887657@students.niu.edu>
2022-11-30 22:12:38 -0600

    added logo back to home

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Home.css";
-// import Logo from "../logo/3B-logos_black.png";
+import Logo from "../logo/3B-logos_black.png";
 import { Link } from "react-router-dom";
 
 function Home() {
@@ -8,44 +8,58 @@ function Home() {
         <div className="home_background">
             <div className="home">
                 <div className="home__flex">
+                    <div className="home__logoContainer">
+                        <img src={Logo} alt="Logo" className="home__logo" />
+                    </div>
                     <div className="home__flexColumn">
-                        <h1>Welcome Back!</h1>
+                        <h1 className="home__header">Welcome Back!</h1>
                         <h3>We're 3B Plant Repair</h3>
-
                     </div>
                 </div>
                 <div className="cardContainer">
-                    <Link className="card" to={"/NewQuote"} style={{ textDecoration: "none" }}>
-                        <img className="cardImage" src={require("../Assets/image1.png")} alt="" />
+                    <Link
+                        className="card"
+                        to={"/NewQuote"}
+                        style={{ textDecoration: "none" }}
+                    >
+                        <img
+                            className="cardImage"
+                            src={require("../Assets/image1.png")}
+                            alt=""
+                        />
                         <div className="cardContent">
-                            <div className="cardHeader">
-                                New Quote
-                            </div>
-                            <p className="cardDesc">
-                                Create itemized quotes
-                            </p>
+                            <div className="cardHeader">New Quote</div>
+                            <p className="cardDesc">Create itemized quotes</p>
                         </div>
                     </Link>
-                    <Link className="card" to={"/FinalizeQuote"} style={{ textDecoration: "none" }}>
-                        <img className="cardImage" src={require("../Assets/image3.png")} alt="" />
+                    <Link
+                        className="card"
+                        to={"/FinalizeQuote"}
+                        style={{ textDecoration: "none" }}
+                    >
+                        <img
+                            className="cardImage"
+                            src={require("../Assets/image3.png")}
+                            alt=""
+                        />
                         <div className="cardContent">
-                            <div className="cardHeader">
-                                Finalize Quote
-                            </div>
-                            <p className="cardDesc">
-                                Edit and finalize quotes
-                            </p>
+                            <div className="cardHeader">Finalize Quote</div>
+                            <p className="cardDesc">Edit and finalize quotes</p>
                         </div>
                     </Link>
-                    <Link className="card" to={"/SanctionQuote"} style={{ textDecoration: "none" }}>
-                        <img className="cardImage" src={require("../Assets/image2.png")} alt="" />
+                    <Link
+                        className="card"
+                        to={"/SanctionQuote"}
+                        style={{ textDecoration: "none" }}
+                    >
+                        <img
+                            className="cardImage"
+                            src={require("../Assets/image2.png")}
+                            alt=""
+                        />
                         <div className="cardContent">
-                            <div className="cardHeader">
-                                Sanction Quote
-                            </div>
-                            <p className="cardDesc">
-                                Process finalized quotes
-                            </p>
+                            <div className="cardHeader">Sanction Quote</div>
+                            <p className="cardDesc">Process finalized quotes</p>
                         </div>
                     </Link>
                 </div>
